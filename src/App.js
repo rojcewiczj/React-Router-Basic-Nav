235a0732574ef2fe5b817adcec0190f6408d5366
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-08-19 13:57:28 -0500

    got route paths and links working

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import './App.css';
 import ReactDOM from "react-dom";
 import { Home, About, Contact, Navigation } from './components';
-import {BrowserRouter} from "react-router-dom";
+import { Route } from "react-router-dom";
+import {BrowserRouter as Router} from "react-router-dom";
 const App = () => (
   <div>
+    <Router>
     <Navigation />
+      <Route path ="/Home" component={Home} />
+      <Route path ="/contact" component={Contact} />
+       <Route path ="/about" component={About} />
+       </Router>
   </div>
+
 );
 
 export default App;
 
 const rootElment = document.getElementById('root');
-ReactDOM.render (<BrowserRouter><App /></BrowserRouter>, rootElment);
+ReactDOM.render (<Router><App /></Router>, rootElment);
+
